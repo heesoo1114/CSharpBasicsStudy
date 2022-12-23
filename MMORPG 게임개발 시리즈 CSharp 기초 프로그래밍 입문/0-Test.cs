2726465fc57dc95fafbde626c11411d30a544c8f
heesoo1114 <improveme7@gmail.com>
2022-12-23 13:05:38 +0900

    ref and out
    ref를 사용하는 게 좋은지 안 좋은지 이유 추가 설명하기

--- MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/0-Test.cs
+++ MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/0-Test.cs
@@ -4,9 +4,9 @@ namespace MMORPG_게임개발_시리즈_CSharp_기초_프로그래밍_입문
 {
     internal class Test
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("add");
         }
     }
 }
