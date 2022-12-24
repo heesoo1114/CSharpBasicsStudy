645829fc2bca5c4433d83c294d6df2fb2380e0b7
heesoo1114 <improveme7@gmail.com>
2022-12-24 21:05:20 +0900

    클래스 형식 변환, 문자열

--- MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/클래스 형식 변환.cs
+++ MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/클래스 형식 변환.cs
@@ -4,9 +4,39 @@ namespace MMORPG_게임개발_시리즈_CSharp_기초_프로그래밍_입문
 {
     internal class Test
     {
+        class Player
+        {
+            protected int hp;
+            protected int attack;
+        }
+
+        class Knight : Player
+        {
+
+        }
+
+        class Mage : Player
+        {
+            public int mp;
+        }
+
+        static void Entergame(Player player)
+        {
+            bool isMage = player is Mage; // player의 타입이 Mage인지 불값으로 확인
+
+            Mage mage = player as Mage; // player의 타입이 Mage라면 mage에 대입하고 아니면 null 대입
+            if (mage != null)
+            {
+                mage.mp = 10;
+            }
+        }
+
         static void Main()
         {
-            
+            Knight knight = new Knight();
+            Mage mage = new Mage();
+
+            Entergame(knight);
         }
     }
 }
