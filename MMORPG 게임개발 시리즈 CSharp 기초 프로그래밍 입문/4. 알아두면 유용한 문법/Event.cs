0a35a7ab195ff359a7dd2050a4f13491a987d79c
heesoo1114 <improveme7@gmail.com>
2022-12-25 16:44:03 +0900

    알아두면유용한문법 1
    Generic
    Delegate
    Event
    Interface
    Nullable
    Property

---
+++ MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/4. 알아두면 유용한 문법/Event.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MMORPG_게임개발_시리즈_CSharp_기초_프로그래밍_입문
+{
+    class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+            {
+                return;
+            }
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if (info.Key == ConsoleKey.A)
+            {
+                InputKey();
+            }
+        }
+    }
+
+    internal class Test
+    {
+        static void OnInputTest()
+        {
+            Console.WriteLine("Input Received!");
+        }
+
+        static void Main()
+        {
+            InputManager inputManager = new InputManager();
+
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
+        }
+    }
+}
--- MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/Reflection.cs
+++ MMORPG 게임개발 시리즈 CSharp 기초 프로그래밍 입문/Reflection.cs
@@ -6,7 +6,7 @@ namespace MMORPG_게임개발_시리즈_CSharp_기초_프로그래밍_입문
     {
         static void Main()
         {
-            Console.WriteLine("add");
+            
         }
     }
 }
