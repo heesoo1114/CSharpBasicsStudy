5eb8ee8a88078a94ae6f2f985ed5bb50b512a32d
heesoo1114 <improveme7@gmail.com>
2023-01-03 01:32:38 +0900

    ~BFS

--- MMORPG 게임개발 시리즈 CSharp  자료구조와 알고리즘/BFS (너비우선탐색).cs
+++ MMORPG 게임개발 시리즈 CSharp  자료구조와 알고리즘/BFS (너비우선탐색).cs
@@ -25,6 +25,41 @@ namespace MMORPG_게임개발_시리즈_CSharp__자료구조와_알고리즘
             new List<int>() { 4 },
         };
 
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+            int[] parent = new int[6];
+            int[] distance = new int[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0) // 인접하지 않으면
+                    {
+                        continue;
+                    }
+                    if (found[next]) // 이미 발견한 얘라면
+                    {
+                        continue;
+                    }
+
+                    q.Enqueue(next); // 넣어주고
+                    found[next] = true; // 발견함으로 변경
+                    parent[next] = now; // 부모 설정
+                    distance[next] = distance[now + 1]; // 거리 계산
+                }
+            }
+        }
         
     }
 
@@ -32,7 +67,8 @@ namespace MMORPG_게임개발_시리즈_CSharp__자료구조와_알고리즘
     {
         static void Main(string[] args)
         {
-
+            Graph graph = new Graph();
+            graph.BFS(0);
         }
     }
 }
