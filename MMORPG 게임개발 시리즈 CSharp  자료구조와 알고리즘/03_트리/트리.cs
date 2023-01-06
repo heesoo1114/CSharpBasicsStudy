0fd6f95d0cf86a80f55ca4f161a1969d8c2f26bc
Yoon HeeSoo <improveme7@gmail.com>
2023-01-06 09:00:02 +0900

    ~트리구현연습

--- MMORPG 게임개발 시리즈 CSharp  자료구조와 알고리즘/03_트리/트리.cs
+++ MMORPG 게임개발 시리즈 CSharp  자료구조와 알고리즘/03_트리/트리.cs
@@ -3,11 +3,86 @@ using System.Collections.Generic;
 
 namespace MMORPG_게임개발_시리즈_CSharp__자료구조와_알고리즘
 {
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+    }
+
     class Sample
     {
+        // 생성
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 개발실" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "디자인팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "전투" });
+                    node.Children.Add(new TreeNode<string>() { Data = "경제" });
+                    node.Children.Add(new TreeNode<string>() { Data = "스토리" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "프로그래밍팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "서버" });
+                    node.Children.Add(new TreeNode<string>() { Data = "클라" });
+                    node.Children.Add(new TreeNode<string>() { Data = "엔진" });
+                    root.Children.Add(node);
+                }
+
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "아트팀" };
+                    node.Children.Add(new TreeNode<string>() { Data = "배경" });
+                    node.Children.Add(new TreeNode<string>() { Data = "캐릭터" });
+                    root.Children.Add(node);
+                }
+                return root;
+            }
+        }
+
+        // 출력
+        static void PrintTree(TreeNode<string> root)
+        {
+            // 접근
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Children)
+            {
+                PrintTree(child);
+            }
+        }
+
+        // 높이
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+
+            foreach (TreeNode<string> child in root.Children)
+            {
+                int newHeight = GetHeight(child) + 1;
+
+                if (height < newHeight)
+                {
+                    height = newHeight;
+                }
+
+                // 위 코드와 아래 코드는 같은 역할을 수행
+
+                height = Math.Max(height, newHeight);
+
+                // Math.Max: 데이터 2개 중 더 큰 값을 반환
+            }
+
+            return height;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("dkdj");
+            TreeNode<string> root = MakeTree();
+            PrintTree(root);
+            Console.WriteLine(GetHeight(root));
         }
     }
 }
